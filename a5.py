a80a929ae9e86e591c476a190db064d01afb877c
Nickscon <nzhang@cps.edu>
2025-11-14 10:10:55 -0600

    DFS vs BFS assignment modifications

--- a5.py
+++ a5.py
@@ -1,4 +1,5 @@
 import copy  # to make a deepcopy of the board
+import time
 from typing import List, Any, Tuple
 
 # import Stack and Queue classes for BFS/DFS
@@ -184,10 +185,16 @@ def DFS(state: Board) -> Board:
     """
     the_stack = Stack()
     the_stack.push(state)
+    iterations = 0 
+    start_time = time.time()
 
     while not the_stack.is_empty():
-        current_board: Board = the_stack.pop()
+        iterations += 1
+        current_board = the_stack.pop()
         if current_board.goal_test():
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"DFS took {iterations} iterations in {elapsed_time: .4f} seconds")
             return current_board
         if not current_board.failure_test():
             row, col = current_board.find_most_constrained_cell()
@@ -214,9 +221,16 @@ def BFS(state: Board) -> Board:
     """
     the_queue = Queue()
     the_queue.push(state)
+    iterations = 0
+    start_time = time.time()
+
     while not the_queue.is_empty():
-        current_board: Board = the_queue.pop()
+        iterations += 1
+        current_board = the_queue.pop()
         if current_board.goal_test():
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            print(f"BFS took {iterations} iterations in {elapsed_time: .4f} seconds")
             return current_board
         if not current_board.failure_test():
             row, col = current_board.find_most_constrained_cell()
